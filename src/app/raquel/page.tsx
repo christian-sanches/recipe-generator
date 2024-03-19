2ec7be59a7115cd0b7f1b444cf7fd3ae5162260f
Christian Sanches <christian@endless-point.org>
2024-03-19 00:07:55 -0300

    raquel pdf loading successfully

---
+++ src/app/raquel/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Typography } from "antd";
+import { PDFDocument } from "pdf-lib";
+import { useCallback, useEffect, useRef } from "react";
+import styles from "./page.module.css";
+
+const { Text, Title } = Typography;
+
+export default function RaquelHome() {
+  const visualizer = useRef<HTMLIFrameElement>(null);
+  const pdfCached = useRef<ArrayBuffer | null>(null);
+
+  const getPdfDoc = useCallback(async () => {
+    if (!pdfCached.current) {
+      const pdfBuffer = await fetch("/pdf/raquel/v1-min.pdf", {
+        cache: "force-cache",
+      }).then((r) => r.arrayBuffer());
+
+      pdfCached.current = pdfBuffer;
+    }
+
+    const pdfDoc = await PDFDocument.load(pdfCached.current);
+
+    return pdfDoc;
+  }, []);
+
+  const fetchPdf = useCallback(async () => {
+    const pdfDoc = await getPdfDoc();
+
+    const mainPage = pdfDoc.getPage(0);
+
+    const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+    if (visualizer.current) {
+      visualizer.current.src = pdfDataUri;
+    }
+
+  }, [getPdfDoc]);
+
+  useEffect(() => {
+    fetchPdf();
+  }, [fetchPdf]);
+
+  return (
+    <main className={styles.main}>
+      <Title level={1}>Gerador de Recibos - Raquel</Title>
+
+      <iframe className={styles.description} ref={visualizer} />
+
+      <footer>
+        <Text>
+          Feito com 💜 por{" "}
+          <a href="mailto:christian@fleflis.dev">Christian Sanches</a> 👨🏻‍💻
+        </Text>
+      </footer>
+    </main>
+  );
+}
