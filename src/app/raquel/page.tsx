ae7cc0a001d23ab31592301a064aec386822fa74
Christian Sanches <christian@endless-point.org>
2024-03-21 21:35:03 -0300

    raquel text positions

--- src/app/raquel/page.tsx
+++ src/app/raquel/page.tsx
@@ -34,6 +34,68 @@ export default function RaquelHome() {
 
     const mainPage = pdfDoc.getPage(0);
 
+    mainPage.drawText("235,37", {
+      x: 130,
+      y: 215,
+      size: 13,
+    });
+
+    mainPage.drawText(
+      "Pamella Fernanda Pietromonaco de Oliveira Sanches, Christian Gomes de Oliveira Sanches",
+      {
+        x: 90,
+        y: 185,
+        size: 12,
+        maxWidth: mainPage.getWidth() - 120,
+        lineHeight: 15,
+      },
+    );
+
+    mainPage.drawText(
+      "Duzendos e trinta e cinco reais e trinta e sete centavos",
+      {
+        x: 105,
+        y: 152,
+        size: 12,
+        maxWidth: mainPage.getWidth() - 120,
+      },
+    );
+
+    mainPage.drawText(
+      "Atendimento psicológico no mês de Janeiro, no dia 01/01/2024",
+      {
+        x: 100,
+        y: 135,
+        size: 12,
+        maxWidth: mainPage.getWidth() - 120,
+        lineHeight: 15,
+      },
+    );
+
+    mainPage.drawText("Jundiaí", {
+      x: 40,
+      y: 85,
+      size: 12,
+    });
+
+    mainPage.drawText("01", {
+      y: 85,
+      x: 155,
+      size: 12,
+    });
+
+    mainPage.drawText("Janeiro", {
+      y: 85,
+      x: 200,
+      size: 12,
+    });
+
+    mainPage.drawText("24", {
+      y: 85,
+      x: 330,
+      size: 12,
+    });
+
     const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
     if (visualizer.current) {
       visualizer.current.src = pdfDataUri;
